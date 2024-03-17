7daf1eeb8ac38fe2510c6533f7b2fb16962c67f2
R3zn1kov <ldlreznikov@gmail.com>
2024-03-17 23:21:36 +0100

    Connected the form with the backend using Flask

--- app.py
+++ app.py
@@ -1,9 +1,62 @@
-from flask import Flask, render_template
+from datetime import datetime
+from flask import Flask, render_template, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+app.config["SECRET_KEY"] = "myapp123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["MAIL SERVER"] = "smtp.gmail.com"
+app.config["MAIL PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "example@gmail.com"
+app.config["MAIL_PASSWORD"] = "example"
+db = SQLAlchemy(app)
 
-app.run(debug=True, port=5001)
+mail = Mail(app)
+
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        date_obj = datetime.strptime(date, "%Y-%m-%d").date()
+        occupation = request.form["occupation"]
+
+        form = Form(first_name=first_name, last_name=last_name,
+                    email=email, date=date_obj, occupation=occupation)
+        db.session.add(form)
+        db.session.commit()
+
+        message_body = f"Grazie per avermi contattato, {first_name}."
+
+        message = Message(subject="New form submission",
+                          sender=app.config["MAIL_USERNAME"],
+                          recipients=[email],
+                          body=message_body)
+        mail.send(message)
+        flash(f"{first_name} Form compilato con successo", "success")
+
+        return render_template("index.html")
+    else:
+        # Add a return statement for GET request
+        return render_template("index.html")
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5001)
